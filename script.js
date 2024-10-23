aecb92c79b18af32bf28a23980f7f42c194c800b
vinicius-robles <vinicius.robles@gmail.com>
2024-10-23 16:55:39 -0300

    Adiciona uma nova estilização do resultado que só é mostrada quando Calcular é acionado. Botão "novo preço" agora limpa todos os campos da aplicação.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let valorLinha = 0.10;
-let valorParOlhos = 1.00;
+let valorParOlhos = 0.50;
 let valorChaveiro = 1.54;
 let valorHoraProducao = 3.50; //horas
+
+let resultadoCusto = document.getElementById("resultado__custo");
+let resultadoLucro = document.getElementById("resultado__preco");
 
 function botaoCalcular() {
 
     let quantidadeLinha = document.getElementById("quantidade__linha").value;
     let quantidadeParOlhos = document.getElementById("quantidade__par__olhos").value;
     let quantidadeChaveiros = document.getElementById("quantidade__chaveiros").value;
-    let horasProducao = document.getElementById("horas__procucao").value;
+    let horasProducao = document.getElementById("horas__producao").value;
 
     let custoLinha = quantidadeLinha * valorLinha;
     let custoParOlhos = quantidadeParOlhos * valorParOlhos;
@@ -18,11 +21,29 @@ function botaoCalcular() {
     let precoCusto = (custoLinha + custoParOlhos + custoChaveiro + custoProducao);
     let lucro = (precoCusto * 1.3);
 
-    let resultadoCusto = document.getElementById("resultado__custo");
-    resultadoCusto.innerHTML = `O custo total do serviço é: <span style="font-weight: 700;">R$${precoCusto.toFixed(2)}</span>`;
-    let resultadoLucro = document.getElementById("resultado__preco");
-    resultadoLucro.innerHTML = `O preço final deste produto é: <span style="font-weight: 700;">R$${lucro.toFixed(2)}</span>`;
+    resultadoCusto.innerHTML = `<p class="conteudo__resultado__texto" id="resultado__custo">O custo total deste produto é: 
+<span style="font-weight: 700;">R$${precoCusto.toFixed(2)}</span></p>`;
+    resultadoLucro.innerHTML = `<p class="conteudo__resultado__texto" id="resultado__custo">O preço final deste produto é: 
+<span style="font-weight: 700;">R$${lucro.toFixed(2)}</span></p>`;
+    mostrarResultado();
+}
 
-    // console.log(`O custo total do serviço é: R$${precoCusto.toFixed(2)}`);
-    // console.log(`O preço final deste produto é: R$${lucro.toFixed(2)}`); 
+function mostrarResultado() {
+    let resultado = document.getElementById("resultado");
+    if (resultado.classList.contains("mostrar")) {
+        resultado.classList.remove("mostrar");
+    } else {
+        resultado.classList.add("mostrar");
+    }
+
+}
+
+function botaoNovoPreco() {
+    document.getElementById("quantidade__linha").value = "";
+    document.getElementById("quantidade__par__olhos").value = "";
+    document.getElementById("quantidade__chaveiros").value = "";
+    document.getElementById("horas__producao").value = "";
+    resultadoCusto.innerHTML = "";
+    resultadoLucro.innerHTML = "";
+    mostrarResultado();
 }
